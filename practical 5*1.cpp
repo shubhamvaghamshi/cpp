3cc1e2d0c5656739277a86209a8bdc101ae896f9
shubhamvaghamshi <162270392+shubhamvaghamshi@users.noreply.github.com>
2024-03-20 10:05:09 +0530

    Create practical 5*1.cpp

---
+++ practical 5*1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+struct college_info{
+    
+    string college_name;
+    int college_code;
+    string department;
+    int intake;
+};
+
+int main()
+{
+    college_info c1;
+    c1.college_name = "Government Polytechnic Junagadh";
+    c1.college_code = 619;
+    c1.department = "Computer";
+    c1.intake = 79;
+    
+    
+    cout<< "college name :"<<c1.college_name<<endl;
+    cout<< "college code :"<<c1.college_code<<endl;
+    cout<< "department :"<<c1.department<<endl;
+    cout<< "intake :"<<c1.intake<<endl;
+    return 0;
+}
