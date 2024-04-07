3758b963c5b5b216c43acb578099aecf3bb6dd33
shubhamvaghamshi <162270392+shubhamvaghamshi@users.noreply.github.com>
2024-04-07 10:44:01 +0530

    Update practical 5*2.cpp

--- practical 5*2.cpp
+++ practical 5*2.cpp
@@ -20,16 +20,16 @@ int main()
     cin >> e1.basic_salary;
     cout << "Enter DA: " << endl;
     cin >> e1.DA;
-    cout << "Enter Icome tex: " << endl;
+    cout << "Enter Income tex: " << endl;
     cin >> e1.IT;
     cout << "Enter Net salary: " << endl;
     cin >> e1.net_salary;
     
-    cout << "Employee name is: " <<e1.employe_num << endl;
-    cout << "Enter Employee name:" <<e1.employe_name << endl;
+    cout << "Employe name is: " <<e1.employe_num << endl;
+    cout << "Enter Employe name:" <<e1.employe_name << endl;
     cout << "Enter Basic salary:" <<e1.basic_salary << endl;
     cout << "Enter DA: " <<e1.DA << endl;
-    cout << "Enter Icome tex: " <<e1.IT << endl;
+    cout << "Enter Income tex: " <<e1.IT << endl;
     cout << "Enter Net salary: " <<e1.net_salary << endl;
     return 0;
 }
